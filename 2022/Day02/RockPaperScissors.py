de97d4bfff47964199c0118a0f02daae5f15c207
PaulHancock <paul.hancock@curtin.edu.au>
2022-12-04 15:13:17 +0800

    tests for part2

--- 2022/Day02/RockPaperScissors.py
+++ 2022/Day02/RockPaperScissors.py
@@ -31,25 +31,52 @@ guide.
 
 For example, suppose you were given the following strategy guide:
 
-AY
-BX
-CZ
-This strategy guide predicts and recommends the following:
+AY BX CZ This strategy guide predicts and recommends the following:
 
 - In the first round, your opponent will choose Rock (A), and you should choose
 Paper (Y). This ends in a win for you with a score of 8 (2 because you chose
-Paper + 6 because you won). 
+Paper + 6 because you won).
+
 - In the second round, your opponent will choose
 Paper (B), and you should choose Rock (X). This ends in a loss for you with a
-score of 1 (1 + 0). 
+score of 1 (1 + 0).
+
 - The third round is a draw with both players choosing
 Scissors, giving you a score of 3 + 3 = 6. 
 
-In this example, if you were to
-follow the strategy guide, you would get a total score of 15 (8 + 1 + 6).
+In this example, if you were to follow the strategy guide, you would get a
+total score of 15 (8 + 1 + 6).
 
 What would your total score be if everything goes exactly according to your
 strategy guide?
+
+--- Part Two ---
+
+The Elf finishes helping with the tent and sneaks back over to you. "Anyway,
+the second column says how the round needs to end: X means you need to lose, Y
+means you need to end the round in a draw, and Z means you need to win. Good
+luck!"
+
+The total score is still calculated in the same way, but now you need to figure
+out what shape to choose so the round ends as indicated. The example above now
+goes like this:
+
+- In the first round, your opponent will choose Rock (A), and you need the
+  round
+to end in a draw (Y), so you also choose Rock. This gives you a score of 1 + 3
+= 4.
+
+- In the second round, your opponent will choose Paper (B), and you choose
+Rock so you lose (X) with a score of 1 + 0 = 1.
+
+- In the third round, you will
+defeat your opponent's Scissors with Rock for a score of 1 + 6 = 7. 
+
+Now that you're correctly decrypting the ultra top secret strategy guide, you
+would get a total score of 12.
+
+Following the Elf's instructions for the second column, what would your total
+score be if everything goes exactly according to your strategy guide?
 """
 
 SYMBOLS = 'rps'
@@ -131,11 +158,30 @@ def test_part1():
     return True
 
 
+def part2(data):
+    return 
+
+
+def test_part2():
+    data = open('test1.txt').readlines()
+    answer = 12
+    result = part2(data)
+    if answer != result:
+        raise AssertionError(f"Score should be {answer} not {result}")
+    print("TEST: part2, PASS")
+    return True
+
+
 def main():
     if test_part1():
         data = open('input1.txt').readlines()
         score = part1(data)
         print(f"Part 1 score is {score}")
+    
+    if test_part2():
+        data = open('input1.txt').readlines()
+        score = part2(data)
+        print(f"Part 2 score is {score}")
         
     return
 
