61dcf9b35b2fdf0d8bed6831aba8253073b1b1d5
PaulHancock <paul.hancock@curtin.edu.au>
2022-12-05 13:15:48 +0800

    solved

--- 2022/Day03/RucksackReorganization.py
+++ 2022/Day03/RucksackReorganization.py
@@ -1,3 +1,4 @@
+from itertools import zip_longest
 """
 --- Day 3: Rucksack Reorganization --- 
 
@@ -60,24 +61,132 @@ priority of the item type that appears in both compartments of each rucksack is
 
 Find the item type that appears in both compartments of each rucksack. What is
 the sum of the priorities of those item types?
+""" +\
+"""
+--- Part Two ---
+
+As you finish identifying the misplaced items, the Elves come to you with
+another issue.
+
+For safety, the Elves are divided into groups of three. Every Elf carries a
+badge that identifies their group. For efficiency, within each group of three
+Elves, the badge is the only item type carried by all three Elves. That is, if
+a group's badge is item type B, then all three Elves will have item type B
+somewhere in their rucksack, and at most two of the Elves will be carrying any
+other item type.
+
+The problem is that someone forgot to put this year's updated authenticity
+sticker on the badges. All of the badges need to be pulled out of the rucksacks
+so the new authenticity stickers can be attached.
+
+Additionally, nobody wrote down which item type corresponds to each group's
+badges. The only way to tell which item type is the right one is by finding the
+one item type that is common between all three Elves in each group.
+
+Every set of three lines in your list corresponds to a single group, but each
+group can have a different badge item type. So, in the above example, the first
+group's rucksacks are the first three lines:
+
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+
+And the second group's rucksacks are the next three lines:
+
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+
+In the first group, the only item type that appears in all three rucksacks is
+lowercase r; this must be their badges. In the second group, their badge item
+type must be Z.
+
+Priorities for these items must still be found to organize the sticker
+attachment efforts: here, they are 18 (r) for the first group and 52 (Z) for
+the second group. The sum of these is 70.
+
+Find the item type that corresponds to the badges of each three-Elf group. What
+is the sum of the priorities of those item types?
 """
 
+def priority(item):
+  # a =1 .... z=26
+  score = ord(item.lower()) - ord('a')+1
+  # A = 26  ... Z=52
+  if item.isupper():
+    score += 26
+  return score
+
+def get_sack_score(rucksack):
+  middle = len(rucksack)//2
+  left = set(rucksack[:middle])
+  right = set(rucksack[middle:])
+  common = left.intersection(right)
+  score = priority(common.pop())
+  return score
+
 def part1(data):
-    return
+  score = 0
+  for l in data:
+    score += get_sack_score(l)
+  return score
 
 def test_part1():
-    return
+  data = open('test1.txt').readlines()
+  answer = 157
+  result = part1(data)
+  if not answer == result:
+    raise AssertionError(f"Part1 FAIL: answer is {answer} not {result}")
+  else:
+    print("Part1 PASS")
+  return True
 
+
+def get_group_badge(e1,e2,e3):
+  # turn the rucksack into a set of chars and strip any \n
+  elf1 = set(e1.strip())
+  elf2 = set(e2.strip())
+  elf3 = set(e3.strip())
+  # find the common item
+  common = elf1.intersection(elf2).intersection(elf3)
+  return common
+
+
+def take_three(iterable):
+  for i in range(0,len(iterable), 3):
+    yield iterable[i:i+3]
+
+def part2(data):
+  score = 0
+  for e1,e2,e3 in take_three(data):
+    common = get_group_badge(e1,e2,e3)
+    score += priority(common.pop())
+  return score
+
+
+def test_part2():
+  data = open('test1.txt').readlines()
+  answer = 70
+  result = part2(data)
+  if not answer == result:
+    raise AssertionError(f"Part2 FAIL: answer is {answer} not {result}")
+  else:
+    print("Part2 PASS")
+  return True
 
 def main():
-    if test_part1():
-        data = open('input1.txt').readlines()
-        score = part1(data)
-        print(f"Part 1 score is {score}")
-    
-        
-    return
+  if test_part1():
+    data = open('input1.txt').readlines()
+    score = part1(data)
+    print(f"Part 1 score is {score}")
+
+  if test_part2():
+    data = open('input1.txt').readlines()
+    score = part2(data)
+    print(f"Part 2 score is {score}")
+      
+  return
 
 
 if __name__ == "__main__":
-    main()
+  main()
