bb0de04707e6636645f0bc3d57ad92c524fb2118
PaulHancock <paul.hancock@curtin.edu.au>
2022-12-06 15:41:17 +0800

    part2 solution

--- 2022/Day01/CalorieCounting.jl
+++ 2022/Day01/CalorieCounting.jl
@@ -16,10 +16,43 @@ function part1(data)
     return best_cals
 end
 
-@assert part1(readlines(open("test1.txt"))) == 24000
+function part2(data)
+    elf_cals = []
+    this_cals = 0
 
-open("part1.txt") do f
-    # read till end of file
-    data = readlines(f)
-    println("Part 1 is $(part1(data))")
-  end
+    for line in data
+        l = strip(line)
+        if length(l)>0
+            this_cals += parse(Int64,l)
+        else
+            push!(elf_cals, this_cals)
+            this_cals = 0
+        end
+    end
+    push!(elf_cals, this_cals)
+    top3 = sort(elf_cals)[end-2:end]
+    return sum(top3)
+end
+
+function main()
+    @assert part1(readlines(open("test1.txt"))) == 24000
+
+    open("part1.txt") do f
+        # read till end of file
+        data = readlines(f)
+        println("Part 1 is $(part1(data))")
+    end
+
+    @assert part2(readlines(open("test1.txt"))) == 45000
+
+    open("part1.txt") do f
+        # read till end of file
+        data = readlines(f)
+        println("Part 2 is $(part2(data))")
+    end
+
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    main()
+end
